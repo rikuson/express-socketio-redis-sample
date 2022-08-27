2820b4dc0f158ad398e52fcf7749139892d780e4
Riku Takeuchi <rikuson@users.noreply.github.com>
2022-08-28 08:49:27 +0900

    feat: Add tailwind

--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -1,5 +1,8 @@
 import { io } from 'https://cdn.socket.io/4.5.1/socket.io.esm.min.js';
 
+const publishing = document.getElementById('publishing');
+const subscribing = document.getElementById('subscribing');
+
 const socket = io({
   query: { roomId: ROOM_ID },
 });
@@ -12,13 +15,10 @@ socket.on('create-room', (room) => {
 socket.on('join-room', (room, id) => {
   console.log(`socket ${id} has joined room ${room}`);
 });
-socket.on('message', (message) => {
-  console.log(`subscribe: ${message}`);
-  subscribing.value = message;
+socket.on('message', (msg) => {
+  console.log(`subscribe: ${msg}`);
+  addMessage(msg);
 });
-
-const publishing = document.getElementById('publishing');
-const subscribing = document.getElementById('subscribing');
 
 function onSubmit(e) {
   event.preventDefault();
@@ -26,6 +26,15 @@ function onSubmit(e) {
   console.log(`publish: ${val}`);
   socket.emit('message', val);
   publishing.value = '';
+}
+
+function addMessage(msg) {
+  const div = document.createElement('div');
+  div.className = 'p-2 border-b';
+  const i = document.createElement('i');
+  i.className = 'fa-solid fa-up-long mr-3 text-gray-400';
+  div.append(i, msg);
+  subscribing.append(div);
 }
 
 window.onSubmit = onSubmit;
