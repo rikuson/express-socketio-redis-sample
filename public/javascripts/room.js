e3c83d6690b29034f4ad4469724c3d531d77bb8b
Riku Takeuchi <rikuson@users.noreply.github.com>
2022-08-28 09:15:29 +0900

    chore: Update logging

--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -7,33 +7,32 @@ const socket = io({
   query: { roomId: ROOM_ID },
 });
 socket.on('connect', () => {
-  console.log('socket is connected');
+  log('down', 'connect', 'socket is connected');
 });
 socket.on('create-room', (room) => {
-  console.log(`room ${room} was created`);
+  log('down', 'create-room', `room ${room} was created`);
 });
 socket.on('join-room', (room, id) => {
-  console.log(`socket ${id} has joined room ${room}`);
+  log('down', 'join-room', `socket ${id} has joined room ${room}`);
 });
 socket.on('message', (msg) => {
-  console.log(`subscribe: ${msg}`);
-  addMessage(msg);
+  log('down', 'message', msg);
 });
 
 function onSubmit(e) {
   event.preventDefault();
   const val = publishing.value;
-  console.log(`publish: ${val}`);
   socket.emit('message', val);
+  log('up', 'message', val);
   publishing.value = '';
 }
 
-function addMessage(msg) {
+function log(type, evt, msg) {
   const div = document.createElement('div');
   div.className = 'p-2 border-b';
   const i = document.createElement('i');
-  i.className = 'fa-solid fa-up-long mr-3 text-gray-400';
-  div.append(i, msg);
+  i.className = `fa-solid fa-${type}-long mr-3 text-gray-400`;
+  div.append(i, `${evt}: ${msg}`);
   subscribing.append(div);
 }
 
